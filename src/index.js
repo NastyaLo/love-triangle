0ee2335cc7ff2c47f7bb3c3828c9c4cd627a1ab8
NastyaLo <loyko2802@gmail.com>
2018-09-13 11:45:42 +0300

    Fixind coincidence of indexes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
+ * @returns currentItember of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
     var triangles = 0;
 
     for (let i = 0; i < preferences.length; i++) {
-        let num = preferences[i];
+        let currentItem = preferences[i];
         let indexes = [];
 
-        if (num == 0) continue;
+        if (currentItem == 0) continue;
 
         for(let j = 0; j < 3; j++) {
-            indexes.push(num - 1);
-            num = preferences[num - 1];
+            indexes.push(currentItem - 1);
+            currentItem = preferences[currentItem - 1];
         }
 
-        if(preferences[i] == num) {
-            triangles++;
-            for(let k = 0; k < preferences.length; k++) {
-                if (indexes.indexOf(k) != -1) preferences[k] = 0;
+        if(preferences[i] == currentItem) {
+            let checkIndexes = indexes;
+            let checkIndexesСoincidence = 0;
+            checkIndexes.sort();
+
+            for(let t = 0; t < checkIndexes.length; t++) {
+                if (checkIndexes[t] == checkIndexes[t + 1]) {
+                    checkIndexesСoincidence++;
+                }
             }
-        } else break;
+
+            if (!checkIndexesСoincidence) {
+                triangles++;
+                for(let k = 0; k < preferences.length; k++) {
+                    if (indexes.indexOf(k) != -1) preferences[k] = 0;
+                }
+            }
+        }
     }
 
     return triangles;
